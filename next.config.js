bb3cf2f116cef35a5f582296b065e2675115bc92
prabesh95 <peter.prabeshdahal1995@gmail.com>
2025-07-30 21:27:44 +0545

    versel.json

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // keep static export enabled
-  basePath: '/portfolio_new_2025', // base URL path
-  assetPrefix: '/portfolio_new_2025/', // prefix for static assets like JS, CSS
+ // base URL path
+  // prefix for static assets like JS, CSS
   eslint: {
     ignoreDuringBuilds: true,
   },
